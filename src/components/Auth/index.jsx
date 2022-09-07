9a473960222acb35423bc50d806631a39bb79fb5
Jerome-Baille <baille.jerome@gmail.com>
2022-09-07 19:29:26 +0200

    feat(auth): add loading spinner to auth
    Add a loading spinner on login, logout and register form

--- src/components/Auth/index.jsx
+++ src/components/Auth/index.jsx
@@ -13,10 +13,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 /* Services */
 import { login, logout, register } from '../../services/auth';
+import LoadingSpinner from '../LoadingSpinner';
 
 
 export default function Auth() {
   const navigate = useNavigate()
+  const [isLoaded, setIsLoaded] = useState(true);
   const { isAuth, setIsAuth, setUser } = useContext(AuthContext);
 
   const [registered, setRegistered] = useState(true);
@@ -37,8 +39,10 @@ export default function Auth() {
 
   const handleLogout = (event) => {
     event.preventDefault();
+    setIsLoaded(false);
 
     logout().then(() => {
+        setIsLoaded(true);
         setToastMessage('Farewell my friend !\nYou will be redirected to the Login page.');
         setShowSuccess(true)
 
@@ -55,9 +59,11 @@ export default function Auth() {
     event.preventDefault();
 
     var user = {...userInfo}
+    setIsLoaded(false);
 
     login(user)
       .then(res => {
+        setIsLoaded(true);
         if(res.status === 200) {
           document.cookie = `token=${res.body.token}`;
           document.cookie = `userId=${res.body.userId}`;
@@ -82,11 +88,13 @@ export default function Auth() {
 
   const handleRegister = (event) => {
     event.preventDefault();
+    setIsLoaded(false);
   
     var newUser = {...userInfo}
   
     register(newUser)
       .then(res => {
+        setIsLoaded(true);
         if(res.status === 201) {
           setToastMessage('Your account has been successfully created !');
           setShowSuccess(true)
@@ -104,58 +112,102 @@ export default function Auth() {
   }
 
 
-  return (
-    <div className="main-container">
-      <Toast 
-          onClose={() => setShowSuccess(false)} 
-          show={showSuccess} 
-          delay={2500} 
-          autohide
-      >
-          <Toast.Header className="toast-header--success">
-              <strong className="me-auto">
-                  <FontAwesomeIcon icon={faCircleCheck} />
-                  <span>  Success !</span>
-              </strong>
-              
-          </Toast.Header>
-          <Toast.Body>{toastMessage}</Toast.Body>
-      </Toast>
+  if(isLoaded){
+    return (
+      <div className="main-container">
+        <Toast 
+            onClose={() => setShowSuccess(false)} 
+            show={showSuccess} 
+            delay={2500} 
+            autohide
+        >
+            <Toast.Header className="toast-header--success">
+                <strong className="me-auto">
+                    <FontAwesomeIcon icon={faCircleCheck} />
+                    <span>  Success !</span>
+                </strong>
+                
+            </Toast.Header>
+            <Toast.Body>{toastMessage}</Toast.Body>
+        </Toast>
 
-      <Toast 
-          onClose={() => setShowError(false)} 
-          show={showError} 
-          delay={2500} 
-          autohide
-      >
-          <Toast.Header className="toast-header--delete">
-              <strong className="me-auto">
-                  <FontAwesomeIcon icon={faCircleExclamation} />
-                  <span>  Error !</span>
-              </strong>
-              
-          </Toast.Header>
-          <Toast.Body>{toastMessage}</Toast.Body>
-      </Toast>
-      <div className="connexion-container">
-        
-        {isAuth ? 
-            <section id="logout-form">
-              <h1>Logout</h1>
-              <p>Are you sure you want to log out ?</p>
+        <Toast 
+            onClose={() => setShowError(false)} 
+            show={showError} 
+            delay={2500} 
+            autohide
+        >
+            <Toast.Header className="toast-header--delete">
+                <strong className="me-auto">
+                    <FontAwesomeIcon icon={faCircleExclamation} />
+                    <span>  Error !</span>
+                </strong>
+                
+            </Toast.Header>
+            <Toast.Body>{toastMessage}</Toast.Body>
+        </Toast>
+        <div className="connexion-container">
+          
+          {isAuth ? 
+              <section id="logout-form">
+                <h1>Logout</h1>
+                <p>Are you sure you want to log out ?</p>
 
-              <Button variant="danger" onClick={handleLogout}>
-                  Logout
-              </Button>
-            </section> 
-        : (registered? 
-            <section id="login-form" className="form-container">
-              <h1>Login</h1>
-                <Form onSubmit={handleLogin} validated>
+                <Button variant="danger" onClick={handleLogout}>
+                    Logout
+                </Button>
+              </section> 
+          : (registered? 
+              <section id="login-form" className="form-container">
+                <h1>Login</h1>
+                  <Form onSubmit={handleLogin} validated>
+                    <Form.Group className="mb-3" controlId="formUsername">
+                      <Form.Label>
+                          Username
+                      </Form.Label>
+                      <Form.Control 
+                            required
+                            type="text" 
+                            placeholder="Enter username"
+                            name="username"
+                            onChange={handleChange} 
+                      />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formPassword">
+                      <Form.Label>
+                          Password
+                      </Form.Label>
+                      <Form.Control 
+                            required
+                            type="password" 
+                            placeholder="Password" 
+                            name="password"
+                            onChange={handleChange}
+                      />
+                    </Form.Group>
+                    <Button 
+                      variant="primary" 
+                      type="submit"
+                      className="btn-connexion"
+                    >
+                      Login
+                    </Button>
+                  </Form>
+
+                  <div className='alt-connexion'>
+                    <span>Not registered? </span> 
+                    <button onClick={toggleLoginRegister} className='alt-link'>
+                      Create an account
+                    </button>
+                  </div>
+              </section> 
+              : 
+              <section id="register-form" className="form-container">
+                <h1>Register</h1>
+                <Form onSubmit={handleRegister}>
                   <Form.Group className="mb-3" controlId="formUsername">
-                    <Form.Label>
-                        Username
-                    </Form.Label>
+                    <Form.Label>Username</Form.Label>
                     <Form.Control 
                           required
                           type="text" 
@@ -164,11 +216,9 @@ export default function Auth() {
                           onChange={handleChange} 
                     />
                   </Form.Group>
-
+      
                   <Form.Group className="mb-3" controlId="formPassword">
-                    <Form.Label>
-                        Password
-                    </Form.Label>
+                    <Form.Label>Password</Form.Label>
                     <Form.Control 
                           required
                           type="password" 
@@ -182,60 +232,24 @@ export default function Auth() {
                     type="submit"
                     className="btn-connexion"
                   >
-                    Login
+                    Submit
                   </Button>
                 </Form>
-
+    
                 <div className='alt-connexion'>
-                  <span>Not registered? </span> 
+                  <span>Already registered? </span> 
                   <button onClick={toggleLoginRegister} className='alt-link'>
-                    Create an account
+                    Log in
                   </button>
                 </div>
-            </section> 
-            : 
-            <section id="register-form" className="form-container">
-              <h1>Register</h1>
-              <Form onSubmit={handleRegister}>
-                <Form.Group className="mb-3" controlId="formUsername">
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control 
-                        required
-                        type="text" 
-                        placeholder="Enter username"
-                        name="username"
-                        onChange={handleChange} 
-                  />
-                </Form.Group>
-    
-                <Form.Group className="mb-3" controlId="formPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control 
-                        required
-                        type="password" 
-                        placeholder="Password" 
-                        name="password"
-                        onChange={handleChange}
-                  />
-                </Form.Group>
-                <Button 
-                  variant="primary" 
-                  type="submit"
-                  className="btn-connexion"
-                >
-                  Submit
-                </Button>
-              </Form>
-  
-              <div className='alt-connexion'>
-                <span>Already registered? </span> 
-                <button onClick={toggleLoginRegister} className='alt-link'>
-                  Log in
-                </button>
-              </div>
-            </section>
-            )}
+              </section>
+              )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <LoadingSpinner />
+    )
+  }
 }
